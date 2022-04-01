12c59e96b7025766862943324f75dbe8a6d1aa5e
Chas Nolte <67934694+noltechas@users.noreply.github.com>
2022-04-01 18:53:40 -0500

    Add files via upload

---
+++ src/SimpleFormInsert.java
@@ -0,0 +1,76 @@
+
+/**
+ * @file SimpleFormInsert.java
+ */
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SimpleFormInsert")
+public class SimpleFormInsert extends HttpServlet {
+   private static final long serialVersionUID = 1L;
+
+   public SimpleFormInsert() {
+      super();
+   }
+
+   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	   String firstName = request.getParameter("firstName");
+	   String lastName = request.getParameter("lastName");
+	   String time = request.getParameter("time");
+	   String date = request.getParameter("date");
+	   String event = request.getParameter("event");
+
+      Connection connection = null;
+      String insertSql = " INSERT INTO NolteProjectTable (id, FIRSTNAME, LASTNAME, TIME, DATE, EVENT) values (default, ?, ?, ?, ?, ?)";
+
+      try {
+         DBConnection.getDBConnection();
+         connection = DBConnection.connection;
+         PreparedStatement preparedStmt = connection.prepareStatement(insertSql);
+         preparedStmt.setString(1, firstName);
+         preparedStmt.setString(2, lastName);
+         preparedStmt.setString(3, time);
+         preparedStmt.setString(4, date);
+         preparedStmt.setString(5, event);
+         preparedStmt.execute();
+         connection.close();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+
+      // Set response content type
+      response.setContentType("text/html");
+      PrintWriter out = response.getWriter();
+      String title = "Insert Data to DB table";
+      String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+      out.println(docType + //
+            "<html>\n" + //
+            "<head><title>" + title + "</title></head>\n" + //
+            "<body bgcolor=\"#f0f0f0\">\n" + //
+            "<h2 align=\"center\">" + title + "</h2>\n" + //
+            "<ul>\n" + //
+
+            "  <li><b>Name</b>: " + firstName + " " + lastName + "\n" + //
+            "  <li><b>Event</b>: " + event + "\n" + //
+            "  <li><b>Time</b>: " + time + "\n" + //
+            "  <li><b>Date</b>: " + date + "\n" + //
+
+            "</ul>\n");
+
+      out.println("<a href=/NolteTechProject/simpleFormSearch.html>Search Data</a> <br>");
+      out.println("</body></html>");
+   }
+
+   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+      doGet(request, response);
+   }
+
+}
